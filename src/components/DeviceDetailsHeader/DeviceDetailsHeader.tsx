584d41cbf4648d826ad4bb831f0cb3fa136f2e13
SvetaShmalko <57237685+SvetaShmalko@users.noreply.github.com>
2020-03-31 16:10:46 +0300

    Revert "device details header has been updated"

--- src/components/DeviceDetailsHeader/DeviceDetailsHeader.tsx
+++ src/components/DeviceDetailsHeader/DeviceDetailsHeader.tsx
@@ -1,56 +1,15 @@
-import React, { Component } from 'react';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import React from 'react';
 import './DeviceDetailsHeader.scss';
 
-const options = [
-    'None',
-    'Atria'
-  ];
 
-class DeviceDetailsHeader extends Component {
-    state ={
-        show: false,
-        anchorMenu: null
-    }
-
-    private onBackClick = () => {
-        console.log('back')
-    }
-
-    private onOptionsClick = (event: React.MouseEvent<HTMLElement>) => {
-        this.setState({
-            show: !this.state.show,
-            anchorMenu: event.currentTarget
-        })
-      };
-
-    private handleClose = () => {
-        this.setState({
-            show: !this.state.show
-        })
-      };
-
-      render() {
-
+const DeviceDetailsHeader = () => {
     return (
-        <header className='device-details-header'>
-            <div className='wrapper'>
-                <button className='action-btn' type='button' onClick={this.onBackClick}><ArrowBackIcon /></button>
-                <button className='action-btn options' type='button' onClick={this.onOptionsClick}><MoreVertIcon /></button>
-                    <Menu anchorEl={this.state.anchorMenu} open={this.state.show} onClose={this.handleClose}>
-                        {options.map(option => (
-                        <MenuItem key={option} selected={option === 'Pyxis'} onClick={this.handleClose}>
-                            {option}
-                        </MenuItem>
-                        ))}
-                    </Menu>
-            </div>
+        <header className='header'>
+        <div className='wrapper'>
+            <h2>Home</h2>
+        </div>
       </header>
     );
-}
 }
 
 export default DeviceDetailsHeader;
