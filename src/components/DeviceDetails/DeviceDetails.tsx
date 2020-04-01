bdd63d0cb30fc57e311f40a9c7353457f4bed373
Dima <dim.kry.lis@gmail.com>
2020-04-02 00:01:43 +0300

    device details updated

--- src/components/DeviceDetails/DeviceDetails.tsx
+++ src/components/DeviceDetails/DeviceDetails.tsx
@@ -4,7 +4,9 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Switch from '@material-ui/core/Switch';
-import './DeviceDetails.scss'
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import style from './DeviceDetails.module.scss'
 
 const modesList: string[] = [
     'mode1', 'mode2', 'mode3'
@@ -18,28 +20,36 @@ const DeviceDetails = () => {
     )
 
     return (
-        <div className='device-details'>
-            <div className='device-details__image'>
+        <div className={style.device_details}>
+            <div className={style.device_details__image}>
                 <img src="http://placehold.it/600" alt="sdf" />
             </div>
-            <div className='device-details__content'>
-                <div className='general-info'>
-                    <div className='info'>
+            <div className={style.device_details__content}>
+                <div className={style.general_info}>
+                    <div className={style.info}>
                         <h5>Device Name</h5>
                         <p>Oven</p>
                     </div>
                     <Switch edge="end" />
                 </div>
                 <h6>Temperature</h6>
-                <div className='range'>
+                <div className={style.range}>
                     <h2>100</h2>
-                    <div className='range__buttons'>
-                        <button className='btn__outlined' > - </button>
-                        <button className='btn__outlined' > + </button>
+                    <div className={style.range__buttons}>
+                        <Button 
+                        variant='outlined' 
+                        color='secondary' 
+                        // onClick={this.decrease}
+                        > - </Button>
+                        <Button 
+                        variant='outlined' 
+                        color='secondary' 
+                        // onClick={this.increase}
+                        > + </Button>
                     </div>
                 </div>
                 <h6>Modes</h6>
-                <div className='modes'>
+                <div className={style.modes}>
                     <FormControl fullWidth={true}>
                         <InputLabel>Current Mode</InputLabel>
                         <Select
@@ -51,6 +61,15 @@ const DeviceDetails = () => {
                             {modeItems}
                         </Select>
                     </FormControl>
+                </div>
+                <div className={style.row}>
+                    <h6>Delete device</h6>
+                    <Button 
+                        variant='outlined' 
+                        color='secondary' 
+                        // onClick={this.increase}
+                        ><DeleteIcon />
+                    </Button>
                 </div>
             </div>
         </div>
