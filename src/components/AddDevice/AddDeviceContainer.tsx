90a99374e01b8c5b4c0a136f0640594fe622be7c
Dima <dim.kry.lis@gmail.com>
2020-04-01 22:32:46 +0300

    select device modal added. AddDevice modal updated

--- src/components/AddDevice/AddDeviceContainer.tsx
+++ src/components/AddDevice/AddDeviceContainer.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import AddDevice from './AddDevice';
+import AddDeviceOven from './AddDeviceOven/AddDeviceOven';
+import AddDeviceRobot from './AddDeviceRobot/AddDeviceRobot';
 import style from './AddDeviceContainer.module.scss';
 import { Device } from '../../redux/reducers/deviceReducer'
 import { AddDeviceAction } from '../../redux/actions/deviceActions/deviceActions'
+import SelectDevice from '../SelectDevice/SelectDevice';
 
 interface Props {
     handleToggleDialog: () => void,
@@ -11,19 +13,23 @@ interface Props {
 
 export default class AddDeviceContainer extends Component<Props> {
 
+    state = {
+        content: 0
+    }
+
     componentDidMount() {
         window.addEventListener("keyup", this.handleKeyUp, false);
         document.addEventListener("click", this.handleOutsideClick, false);
         document.body.style.overflow = 'hidden';
-      }
-    
-      componentWillUnmount() {
+    }
+
+    componentWillUnmount() {
         window.removeEventListener("keyup", this.handleKeyUp, false);
         document.removeEventListener("click", this.handleOutsideClick, false);
         document.body.style.overflow = 'unset';
-      }
+    }
 
-      handleKeyUp = (event: { keyCode: number; preventDefault: () => void; }) => {
+    handleKeyUp = (event: { keyCode: number; preventDefault: () => void; }) => {
         if (event.keyCode === 27) {
             event.preventDefault();
             this.props.handleToggleDialog();
@@ -34,12 +40,39 @@ export default class AddDeviceContainer extends Component<Props> {
         if (event.target.classList.contains(`${style.dialog}`)) {
             this.props.handleToggleDialog();
         }
-      }
+    }
+
+    handleContent = (count: number): void => {
+        this.setState({
+            content: count
+        })
+    }
+
+    public renderContent = (): JSX.Element | null => {
+        switch (this.state.content) {
+            case 0:
+                return <SelectDevice 
+                handleToggleDialog={this.props.handleToggleDialog} 
+                handleContent={this.handleContent} />;
+            case 1:
+                return <AddDeviceOven 
+                handleToggleDialog={this.props.handleToggleDialog} 
+                handleContent={this.handleContent} 
+                addDevice={this.props.addDevice} />;
+            case 2:
+                return <AddDeviceRobot 
+                handleToggleDialog={this.props.handleToggleDialog} 
+                handleContent={this.handleContent} 
+                addDevice={this.props.addDevice} />;
+            default:
+                return null;
+        }
+    }
 
     render() {
         return (
             <div className={style.dialog}>
-                <AddDevice handleToggleDialog={this.props.handleToggleDialog} addDevice={this.props.addDevice} />
+                {this.renderContent()}
             </div>
         )
     }
---
+++ src/components/AddDevice/AddDeviceRobot/AddDeviceRobot.tsx
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import { AddDeviceAction } from '../../../redux/actions/deviceActions/deviceActions';
+import { Device } from '../../../redux/reducers/deviceReducer';
+import TextField from '@material-ui/core/TextField';
+import style from './AddDeviceRobot.module.scss';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+
+interface State {
+    name: string,
+    id: number,
+    image: string,
+    temp: {
+        min: number,
+        max: number,
+        current: number,
+        step: number
+    },
+    mode: string,
+    modes: string[]
+}
+
+const initialState: State = {
+    name: '',
+    id: 0,
+    image: 'https://placehold.it/400px',
+    temp: {
+        min: 0,
+        max: 0,
+        current: 0,
+        step: 0
+    },
+    mode: '',
+    modes: [],
+}
+
+interface Props {
+    handleToggleDialog: () => void,
+    addDevice: (p: Device) => AddDeviceAction,
+    handleContent: (count: number) => void
+}
+
+class AddDeviceRobot extends Component<Props, State> {
+    public state: State = initialState;
+
+    private _form = React.createRef<HTMLFormElement>();
+
+    public handleStringInputChange = (event: { currentTarget: { name: string, value: string; }; }) => {
+        this.setState({
+            ...this.state,
+            [event.currentTarget.name]: event.currentTarget.value
+        })
+    }
+
+    public handleModeInputChange = (event: { currentTarget: { value: string; }; }) => {
+        this.setState({
+            mode: event.currentTarget.value
+        })
+    }
+
+    public handleModeInputClick = (e: { preventDefault: () => void; }) => {
+        console.log(this.state.mode)
+        e.preventDefault();
+        this.setState({
+            modes: [...this.state.modes, this.state.mode],
+            mode: ''
+        })
+    }
+
+    private onSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        this.setState({
+            id: Math.round(Math.random() * 100)
+        });
+        this.props.addDevice(this.state);
+    }
+
+    private handleDelete = (mode: string) => {
+        const newModes = this.state.modes;
+        const index: number = this.state.modes.indexOf(mode);
+        newModes.splice(index, 1);
+        this.setState({
+            modes: newModes
+        })
+    }
+
+    private chips = (): JSX.Element[] =>
+        this.state.modes.map((mode, index) => (
+            <Chip key={index} className={style.chip__item} label={mode} onDelete={() => this.handleDelete(mode)} />
+        ))
+
+    render() {
+        return (
+            <div className={style.add_device_dialog__inner}>
+                <h5>Add Device (Robot)</h5>
+                <form className={style.form} ref={this._form}>
+                    <div className={style.general}>
+                        <div className={style.row}>
+                            <TextField
+                                required
+                                fullWidth={true}
+                                type='text'
+                                value={this.state.name}
+                                name='name'
+                                label="Name"
+                                color='secondary'
+                                onChange={this.handleStringInputChange} />
+                        </div>
+                        <div className={style.row}>
+                            <TextField
+                                fullWidth={true}
+                                type='text'
+                                name='image'
+                                label='Image'
+                                color='secondary'
+                                onChange={this.handleStringInputChange} />
+                        </div>
+                    </div>
+                    <h6>Modes</h6>
+                    <div className={style.modes}>
+                        <div className={style.row}>
+                            <TextField
+                                fullWidth={true}
+                                type='text'
+                                name='image'
+                                label='Mode'
+                                color='secondary'
+                                value={this.state.mode}
+                                onChange={this.handleModeInputChange} />
+                            <Button className={style.button} variant="outlined" color="secondary" onClick={this.handleModeInputClick}>+</Button>
+                        </div>
+                        <div className={style.chips}>
+                            {this.chips()}
+                        </div>
+                    </div>
+                    <div className={style.action_buttons}>
+                        <Button color="secondary" onClick={() => this.props.handleContent(0)}>Back</Button>
+                        <div>
+                            <Button color="secondary" onClick={this.props.handleToggleDialog}>Cancel</Button>
+                            <Button
+                                className={style.right}
+                                color="secondary"
+                                type='submit'
+                                disabled={!this.state.name}
+                                onClick={this.onSubmit}>Add Device</Button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default AddDeviceRobot;
