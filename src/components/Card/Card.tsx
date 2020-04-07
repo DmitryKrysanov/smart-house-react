c2be7df0748855a6eb86d5c7ff96f2e2de923ff8
Dima <dim.kry.lis@gmail.com>
2020-04-07 08:14:01 +0300

    converted to the function

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,75 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import style from './Card.module.scss';
 import { Oven, RobotHoover } from '../../redux/reducers/deviceReducer';
 import Switch from '@material-ui/core/Switch';
-import Button from '@material-ui/core/Button';
 
 interface Props {
     device: Oven | RobotHoover,
     deviceToggle: (id: number) => void
 }
 
-class Card extends Component<Props> {
+const Card = (props: Props) => {
 
-    state = {
-        showModal: false
-    }
+    const { device } = props;
 
-    handleToggleDialog = () => {
-        this.setState({
-            showModal: !this.state.showModal
-        })
-    }
-
-    render() {
-        // const {showModal} = this.state;
-        const {device} = this.props;
-        console.log(this.props.device)
-
-        return (
-            <div>
-                <div className={style.card} >
-                    <div className={style.card__image}>
-                        <img src={device.image} alt={device.name} />
+    return (
+        <div>
+            <div className={style.card} >
+                <div className={style.card__image}>
+                    <img src={device.image} alt={device.name} />
+                </div>
+                <div className={style.card__content}>
+                    <div>
+                        <h5>{device.name}</h5>
+                        <p>{props.device.type}</p>
                     </div>
-                    <div className={style.card__content}>
-                        <div>
-                            <h5>{device.name}</h5>
-                            <p>{this.props.device.type}</p>
-                        </div>
-                        <Switch edge="end" onChange={() => {this.props.deviceToggle(this.props.device.id)}} checked={this.props.device.status} />
-                        {/* <button className='btn__outlined' onClick={this.handleToggleDialog}>Details</button>
-                        {showModal ? ReactDOM.createPortal(
-                            <DeviceDetailsDialogContainer device={device} handleToggleDialog={this.handleToggleDialog}/>,
-                            document.getElementById('modal-root') as HTMLInputElement
-                            ) : null  */}
-
-                        {/* <button className='btn__outlined'>Details</button> */}
-                        
-                        {/* {this.props.device.type === 'oven' ? 
-                            <div className={style.range}>
-                                <h2>{this.props.device.temp.current}</h2>
-                                <div className={style.range__buttons}>
-                                    <Button 
-                                    variant='outlined' 
-                                    color='secondary' 
-                                    // onClick={this.decrease}
-                                    > - </Button>
-                                    <Button 
-                                    variant='outlined' 
-                                    color='secondary' 
-                                    // onClick={this.increase}
-                                    > + </Button>
-                                </div>
-                            </div> : null
-                        } */}
-                        
-                        
-                    </div>
+                    <Switch edge="end"
+                        onChange={() => { props.deviceToggle(props.device.id) }}
+                        checked={props.device.status} />
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Card;
