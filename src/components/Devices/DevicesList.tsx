48b07d07fe0fc5aaa45eb1a900413c4c2eec7b4d
Dima <dim.kry.lis@gmail.com>
2020-04-11 14:08:47 +0300

    DevicesList component created

---
+++ src/components/Devices/DevicesList.tsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import { Oven, RobotHoover } from '../../redux/reducers/deviceReducer';
+import { Link } from 'react-router-dom';
+import Card from '../Card/Card';
+import style from './DevicesList.module.scss';
+
+interface Props {
+    devices: Array<Oven | RobotHoover>
+}
+
+const DevicesList = (props: Props) => {
+    const devices = props.devices.map(device => (
+        <Fragment key={device.id}>
+            <Link to={`device/${device.id}`}>
+                <Card device={device} />
+            </Link>
+        </Fragment>));
+
+    return (
+        <div className={style.collection}>{devices}</div>
+    )
+}
+
+export default DevicesList;
