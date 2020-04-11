45fd35c02e7892d92d857ba3ad2f1b261ad71ece
Dima <dim.kry.lis@gmail.com>
2020-04-11 14:10:42 +0300

    Redux logic removed from App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,28 @@
 import React from 'react';
 import './App.scss';
-import DeviceDetails from './components/DeviceDetails/DeviceDetails';
 import { Switch, Route } from 'react-router-dom';
-import { Oven, RobotHoover, DevicesState } from './redux/reducers/deviceReducer';
-import { Dispatch } from './redux/store';
-import { addDevice, turnOffAllDevices, turnOnOffDevice, setDevices, removeDevice, SetCurrentPage } from './redux/actions/deviceActions/deviceActions';
-import { connect } from 'react-redux';
-import { showLoader, hideLoader } from './redux/actions/loaderActions/loaderActions';
 import Devices from './components/Devices/Devices';
+import DevicesHeader from './components/DevicesHeader/DevicesHeader';
+import Filter from './components/Tabs/Filter';
 
-interface ConnectedProps {
-  devices: Array<Oven | RobotHoover>,
-  totalPages: number,
-  page: number,
-  perPage: number
-}
-
-type ComponentProps = ConnectedProps & ReturnType<typeof mapDispatchToProps>;
-
-const App = (props: ComponentProps) => {
+const App = () => {
 
   return (
     <div>
+      {/* <DevicesHeader /> */}
       <div className='wrapper'>
         <div className='content'>
+        {/* <Filter /> */}
           <Switch >
             <Route path='/home/devices'>
-              <Devices
-                devices={props.devices}
-                totalPages={props.totalPages}
-                page={props.page}
-                perPage={props.perPage}
-                addResourse={props.addResourse}
-                offDevices={props.offDevices}
-                deviceToggle={props.deviceToggle}
-                loadDevices={props.loadDevices}
-                showLoader={props.showLoader}
-                hideLoader={props.hideLoader}
-                setCurrentPage={props.setCurrentPage}
-
-              />
+              <Devices />
             </Route>
             <Route path='/home/device/:deviceId'>
-              <DeviceDetails
+              {/* <DeviceDetails
                 devices={props.devices}
                 deviceToggle={props.deviceToggle}
                 removeDevice={props.removeDevice}
-              />
+              /> */}
             </Route>
           </Switch>
         </div>
@@ -55,40 +31,4 @@ const App = (props: ComponentProps) => {
   );
 }
 
-const mapStateToProps = (state: { deviceReducer: DevicesState }): ConnectedProps => {
-  return ({
-    devices: state.deviceReducer.devices,
-    totalPages: state.deviceReducer.totalPages,
-    page: state.deviceReducer.page,
-    perPage: state.deviceReducer.perPage
-  });
-}
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  addResourse: (p: Oven | RobotHoover) => {
-    return dispatch(addDevice(p));
-  },
-  offDevices: () => {
-    return dispatch(turnOffAllDevices());
-  },
-  deviceToggle: (id: number) => {
-    return dispatch(turnOnOffDevice(id));
-  },
-  showLoader: () => {
-    return dispatch(showLoader());
-  },
-  hideLoader: () => {
-    return dispatch(hideLoader());
-  },
-  loadDevices: (p: Array<Oven | RobotHoover>) => {
-    return dispatch(setDevices(p));
-  },
-  removeDevice: (id: number) => {
-    return dispatch(removeDevice(id));
-  },
-  setCurrentPage: (p: number) => {
-    return dispatch(SetCurrentPage(p))
-  }
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
