0969d63853d81eece4bb1de9859dfc4452219c62
Dima <dim.kry.lis@gmail.com>
2020-04-17 12:00:10 +0300

    deleted old imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.scss';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Devices from './components/Devices/Devices';
 import DevicesHeader from './components/DevicesHeader/DevicesHeader'
 import { routes } from './routes';
-import { IconButton } from '@material-ui/core';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import DeviceDetails from './components/DeviceDetails/DeviceDetails';
 
 const App = () => {
@@ -23,25 +21,7 @@ const App = () => {
             </Route>
             <Route path='/home/device/:deviceId'>
               <DeviceDetails />
-              {/* <div>
-                <Link to={routes.allDevices}>
-                  <IconButton> <ArrowBackIcon /> </IconButton>
-                </Link>
-                <br />
-                Details of some device...
-              </div> */}
             </Route>
-
-            {/* <Route path='/home/devices/:deviceId'>
-              <div>
-                <Link to={routes.allDevices}>
-                  <IconButton> Back </IconButton>
-                </Link>
-                <br />
-                Details of some device...
-              </div>
-            </Route> */}
-
           </Switch>
         </div>
       </div>
