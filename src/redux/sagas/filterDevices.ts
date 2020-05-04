4334718d0e496f94ca71187f31a08d9596bc11ba
SvetaShmlako <svetlana.shmalko11@gmail.com>
2020-05-04 12:56:20 +0300

    added request with fetch and deleted axios

--- src/redux/sagas/filterDevices.ts
+++ src/redux/sagas/filterDevices.ts
@@ -1,18 +1,21 @@
 import { takeEvery, call, put } from "redux-saga/effects";
-import { devicesAPI } from "../../api/api";
 import { FILTER_DEVICES } from "../../constants/deviceActions";
 import { FilterDevicesAction, setDevices, setTotalItems } from "../actions/deviceActions/deviceActions";
 import { Oven, RobotHoover } from "../reducers/deviceReducer";
+import { showLoader, hideLoader } from "../actions/loaderActions/loaderActions";
 
-export const filterDevices = (page: number, type: string): Promise<Array<Oven | RobotHoover>> => {
-    return devicesAPI.filter(page, type);
+const filterDevices = (page: number, type: string) => {
+    return fetch(`http://localhost:3001/api/v1/homes/1/devices?page=${page}${type}&perPage=4`)
+        .then(resp => resp.json())
 }
 
 function* workFilterDevices(action: FilterDevicesAction) {
     try {
+        yield put(showLoader());
         const filterData = yield call(filterDevices, action.payload.page, action.payload.type);
         yield put(setDevices(filterData.data));
         yield put(setTotalItems(filterData.totalItems));
+        yield put(hideLoader());
     } catch (error) {
         console.log(error);
     }
